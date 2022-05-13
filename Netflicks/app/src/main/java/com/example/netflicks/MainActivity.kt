7e80bd0845b0c9f90242d1891967fe77039a9649
Robert Artin <robert.artin@endava.com>
2022-05-13 15:06:51 +0300

    Created splash screen composable, added AllertaSencil font & colors for gradient

--- Netflicks/app/src/main/java/com/example/netflicks/MainActivity.kt
+++ Netflicks/app/src/main/java/com/example/netflicks/MainActivity.kt
@@ -3,41 +3,61 @@ package com.example.netflicks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.netflicks.ui.theme.BlackApplication
+import com.example.netflicks.ui.theme.BlueApplication
 import com.example.netflicks.ui.theme.NetflicksTheme
+import com.example.netflicks.ui.theme.WhiteApplication
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NetflicksTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SplashScreenScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SplashScreenScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    0.0f to BlackApplication,
+                    0.3f to BlueApplication,
+                    0.4f to WhiteApplication,
+                    0.6f to WhiteApplication,
+                    0.7f to BlueApplication,
+                    1f to BlackApplication,
+                )
+            )
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            modifier = Modifier.align(Alignment.Center),
+            style = MaterialTheme.typography.h1
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PreviewSplashScreen() {
     NetflicksTheme {
-        Greeting("Android")
+        SplashScreenScreen()
     }
 }
--- Netflicks/app/src/main/java/com/example/netflicks/ui/theme/Color.kt
+++ Netflicks/app/src/main/java/com/example/netflicks/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val BlackApplication = Color(0xFF000000)
+val BlueApplication = Color(0xFF0F205B)
+val WhiteApplication = Color(0xFFFFFFFF)
