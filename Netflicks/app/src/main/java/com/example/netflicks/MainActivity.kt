642330d819674649257526f986cd0bdbc1ba0d9a
Robert Artin <robert.artin@endava.com>
2022-06-08 00:49:33 +0300

    Replaced text with GIF animation which runs once

--- Netflicks/app/src/main/java/com/example/netflicks/MainActivity.kt
+++ Netflicks/app/src/main/java/com/example/netflicks/MainActivity.kt
@@ -1,25 +1,32 @@
 package com.example.netflicks
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.netflicks.ui.theme.BlackApplication
 import com.example.netflicks.ui.theme.BlueApplication
 import com.example.netflicks.ui.theme.NetflicksTheme
 import com.example.netflicks.ui.theme.WhiteApplication
+import com.skydoves.landscapist.glide.GlideImage
+import kotlinx.coroutines.*
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -46,11 +53,31 @@ fun SplashScreenScreen() {
                 )
             )
     ) {
-        Text(
-            text = stringResource(R.string.app_name),
-            modifier = Modifier.align(Alignment.Center),
-            style = MaterialTheme.typography.h1
-        )
+        GlideImage(
+            imageModel = R.raw.intro,
+            contentScale = ContentScale.Fit,
+            requestListener = object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    (resource as GifDrawable).setLoopCount(1)
+
+                    return false
+                }
+            })
     }
 }
 
